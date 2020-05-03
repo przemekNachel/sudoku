250592cc93e0e2bf19f9013b115a54d754c0a271
przemeknachel <przemeknachel@gmail.com>
2020-05-04 01:29:33 +0200

    Add get line

--- test/model/test_board.py
+++ test/model/test_board.py
@@ -4,21 +4,37 @@ import unittest
 
 class TestBoard(unittest.TestCase):
 
-    def test_get(self):
+    def test_get_1(self):
         board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
         expected = "1"
         self.assertEqual(expected, str(board.get(square=9, field=9)))
+
+    def test_get_2(self):
+        board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
         expected = "2"
         self.assertEqual(expected, str(board.get(square=5, field=5)))
+
+    def test_get_3(self):
+        board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
         expected = "7"
         self.assertEqual(expected, str(board.get(square=3, field=3)))
+
+    def test_get_4(self):
+        board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
         expected = "4"
         self.assertEqual(expected, str(board.get(square=4, field=2)))
 
-    def test_get_line(self):
+    def test_get_line_1(self):
         board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
-        expected = "839162547"
-        self.assertEqual(expected, "".join([b.value for b in board.get_line(1)]))
+        field = board.get(4, 4)
+        expected = "785629134"
+        self.assertEqual(expected, "".join([b.value for b in board.get_line(field)]))
+
+    def test_get_line_2(self):
+        board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
+        field = board.get(4, 4)
+        expected = "785629134"
+        self.assertEqual(expected, "".join([b.value for b in board.get_line(field)]))
 
     # def test_update_possible_values(self):
     #     board = src.model.board.Board("...9.......857...3......2819......6.....1.7.8..3....95..1.53.798.....51.6........")
