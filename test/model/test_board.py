60e3b543482bc96b2484c386d604dc2ab4f2d29e
przemeknachel <przemeknachel@gmail.com>
2020-05-04 01:35:48 +0200

    Add get column

--- test/model/test_board.py
+++ test/model/test_board.py
@@ -26,8 +26,8 @@ class TestBoard(unittest.TestCase):
 
     def test_get_line_1(self):
         board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
-        field = board.get(4, 4)
-        expected = "785629134"
+        field = board.get(1, 1)
+        expected = "839162547"
         self.assertEqual(expected, "".join([b.value for b in board.get_line(field)]))
 
     def test_get_line_2(self):
@@ -35,6 +35,18 @@ class TestBoard(unittest.TestCase):
         field = board.get(4, 4)
         expected = "785629134"
         self.assertEqual(expected, "".join([b.value for b in board.get_line(field)]))
+
+    def test_get_column_1(self):
+        board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
+        field = board.get(2, 6)
+        expected = "257891463"
+        self.assertEqual(expected, "".join([b.value for b in board.get_column(field)]))
+
+    def test_get_column_2(self):
+        board = src.model.board.Board("839162547274985316516437892142378965785629134693541728328714659451896273967253481")
+        field = board.get(7, 2)
+        expected = "371489256"
+        self.assertEqual(expected, "".join([b.value for b in board.get_column(field)]))
 
     # def test_update_possible_values(self):
     #     board = src.model.board.Board("...9.......857...3......2819......6.....1.7.8..3....95..1.53.798.....51.6........")
