8119cb37a3ddf5dcf0460200d753d68f55edc089
przemeknachel <przemeknachel@gmail.com>
2020-06-02 01:31:49 +0200

    Add update_possible_values to fields in board.py

--- src/resolver/last_possible.py
+++ src/resolver/last_possible.py
@@ -2,6 +2,7 @@ class LastPossible:
 
     def __init__(self, board):
         self.scan_counter = 0
+        self.committed = 0
         self.board = board
         self.fields_with_last_possible_value = []
 
@@ -13,3 +14,8 @@ class LastPossible:
                     self.fields_with_last_possible_value.append(field)
         self.fields_with_last_possible_value = list(set(self.fields_with_last_possible_value))
         self.scan_counter += 1
+
+    def commit(self):
+        for field in self.fields_with_last_possible_value:
+            field.value = field.possible_values.pop()
+            self.committed += 1
--- test/resolver/test_last_possible.py
+++ test/resolver/test_last_possible.py
@@ -26,6 +26,24 @@ class TestLastPossible(unittest.TestCase):
         self.assertEqual(1, len(resolver.fields_with_last_possible_value))
         self.assertEqual(2, resolver.scan_counter)
 
+    def test_resolve_after_one_scan_1(self):
+        board = Board("23.847965874569231659312487196235874783694512542781396417923658365478129928156743")
+        resolver = LastPossible(board)
+        resolver.scan_all()
+        resolver.commit()
+        self.assertEqual("1", board.get(1, 3).value)
+        self.assertEqual(1, resolver.scan_counter)
+        self.assertEqual(1, resolver.committed)
+
+    def test_resolve_after_one_scan_2(self):
+        board = Board("231847965874569231659312487196235874783694512542781396417923658365478129928156.43")
+        resolver = LastPossible(board)
+        resolver.scan_all()
+        resolver.commit()
+        self.assertEqual("7", board.get(9, 7).value)
+        self.assertEqual(1, resolver.scan_counter)
+        self.assertEqual(1, resolver.committed)
+
 
 if __name__ == '__main__':
     unittest.main()
